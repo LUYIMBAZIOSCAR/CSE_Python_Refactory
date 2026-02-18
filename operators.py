d39c108eb53227fe5a510da515081bc47e50e970
Oscar <luyimbazioscar53@gmail.com>
2026-02-18 10:16:49 +0300

    My first commit

---
+++ operators.py
@@ -0,0 +1,27 @@
+# Mathematical operators
+print(2**2) # makes the number on the right the power of the number on the left
+print(3//2) # rounds down to a whole number
+print(3%2) # returns the remainder of the division
+print(2%3)
+print(10/2) # This divides a number with the other and returns a float
+print(10//2)
+
+# Assignment operators  e.g =, +=, -=, %=
+num=10
+num += 10 # += adds the variable to another number or variable without repetition of the variable
+num += 20 # This is equivalent to num=num + 20
+print(num)
+
+# Comparision operators: They are used to compare values 
+# They return either True or False
+
+print(10==9) # == is equal to
+print(10!=9) # != is the not equal to 
+print(10>=10) # >= is the greater than or equal to
+print(10<=10) # <= is the less than or equal to
+print(10<9) # This the greater than 
+print(10>9) # This is the less than
+
+# Logical operators e.g or,and ,not
+print( not True )
+print(True and False)
